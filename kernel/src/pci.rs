20d22774250b5c1b20815e8c5e655579fc547ea6
zxrs <tanashin@bj.wakwak.com>
2025-02-21 09:17:02 +0900

    todo impl scan all bus

--- kernel/src/pci.rs
+++ kernel/src/pci.rs
@@ -1,7 +1,22 @@
-use crate::Result;
+use crate::{Result, x86};
 
 pub const CONFIG_ADDRESS: u16 = 0x0cf8;
 pub const CONFIG_DATA: u16 = 0x0cfc;
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct Device {
+    bus: u8,
+    device: u8,
+    function: u8,
+    header_thype: u8,
+}
+
+pub static mut DEVICES: [Option<Device>; 32] = [None; 32];
+pub static mut NUM_DEVICES: usize = 0;
+
+pub fn scan_all_bus() -> Result<()> {
+    Ok(())
+}
 
 pub fn make_address(bus: u8, device: u8, function: u8, reg_addr: u8) -> u32 {
     let shl = |x: u32, bits: usize| x << bits;
@@ -11,4 +26,21 @@ pub fn make_address(bus: u8, device: u8, function: u8, reg_addr: u8) -> u32 {
         | shl(device as u32, 11)
         | shl(function as u32, 8)
         | (reg_addr as u32 & 0xfc)
+}
+
+pub fn write_address(address: u32) {
+    x86::io_out32(CONFIG_ADDRESS, address);
+}
+
+pub fn write_data(value: u32) {
+    x86::io_out32(CONFIG_DATA, value);
+}
+
+pub fn read_data() -> u32 {
+    x86::io_in32(CONFIG_DATA)
+}
+
+pub fn read_vendor_id(bus: u8, device: u8, function: u8) -> u16 {
+    write_address(make_address(bus, device, function, 0x00));
+    read_data() as _
 }
--- kernel/src/x86.rs
+++ kernel/src/x86.rs
@@ -4,8 +4,12 @@ pub fn halt() {
     unsafe { asm!("hlt") };
 }
 
-pub fn io_out32(addr: u16, data: u32) -> u32 {
+pub fn io_out32(addr: u16, data: u32) {
+    unsafe { asm!("out {0:x}, eax", in(reg) addr, in("eax") data) };
+}
+
+pub fn io_in32(addr: u16) -> u32 {
     let a;
-    unsafe { asm!("out {0:l}, eax", in(reg) addr, inlateout("eax") data => a ) };
+    unsafe { asm!("in eax {0:x}", in(reg) addr, out("eax") a) };
     a
 }
